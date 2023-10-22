0e756dfc43087006fa33f33b48a887a029801a82
liscandeia <liscandeia523@gmail.com>
2023-10-21 22:27:26 -0300

    Subindo tarefa do módulo 9

---
+++ src/Casting.java
@@ -0,0 +1,13 @@
+
+public class Casting {
+    public static void main(String[] args) {
+        casting();
+    }
+    public static void casting(){
+        long numero = 56;
+        System.out.println(numero);
+        Long numeroConvertido = (Long) numero;
+        System.out.println(numeroConvertido);
+
+    }
+}
